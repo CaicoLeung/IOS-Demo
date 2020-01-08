c371fffe00cb1dd448b933f82a2982eaf84619a1
CaicoLeung <lkk1053190902@gmail.com>
2020-01-09 00:03:18 +0800

    init

--- IOSDemo/ViewController.m
+++ IOSDemo/ViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
-@interface ViewController ()
-
+@interface ViewController ()<UITextFieldDelegate, UITextViewDelegate>
+@property Boolean flag;
+@property (strong, nonatomic)UILabel *label;
 @end
 
 @implementation ViewController
@@ -17,7 +19,63 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.flag = true;
+    CGRect screen = [[UIScreen mainScreen] bounds];
+    CGFloat labelWidth = 180;
+    CGFloat labelHeight = 20;
+    CGFloat labelTopView = 150;
+    CGRect labelFrame =  CGRectMake((screen.size.width - labelWidth) / 2, labelTopView, labelWidth, labelHeight);
+    self.label = [[UILabel alloc] initWithFrame:labelFrame];
+    self.label.text = @"Caico Leung";
+    self.label.textColor = [UIColor greenColor];
+    self.label.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview: self.label];
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    [button setTitle:@"点我啊" forState:UIControlStateNormal];
+    [button setTitle:@"我亮了" forState:UIControlStateHighlighted];
+    CGFloat buttonWidth = 60;
+    CGFloat buttonHeight = 20;
+    CGFloat buttonTopView = 240;
+    button.frame = CGRectMake((screen.size.width - buttonWidth) / 2, buttonTopView, buttonWidth, buttonHeight);
+    [button addTarget:self action:@selector(onButtonOnclick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+    
+    CGFloat textFieldWidth = 236;
+    CGFloat textFieldheight = 30;
+    CGFloat textTopView = 300;
+    CGRect textFieldFrame = CGRectMake((screen.size.width - textFieldWidth) / 2, textTopView, textFieldWidth, textFieldheight);
+    UITextField *textField = [[UITextField alloc] initWithFrame:textFieldFrame];
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    textField.delegate = self;
+    [self.view addSubview:textField];
+//    labelName标签与textField之间的距离
+    CGFloat labelNameTextFieldSpace = 30;
+    UILabel *labelName = [[UILabel alloc] initWithFrame:CGRectMake(textField.frame.origin.x, textField.frame.origin.y - labelNameTextFieldSpace, 51, 21)];
+    labelName.text = @"Name:";
+    [self.view addSubview:labelName];
+    
+    CGFloat textViewWidth = 236;
+    CGFloat textViewHeight = 198;
+    CGFloat textViewTopView = 360;
+    UITextView *textView = [[UITextView alloc] initWithFrame:CGRectMake((screen.size.width - textViewWidth) / 2, textViewTopView, textViewWidth, textViewHeight)];
+    textView.layer.borderWidth = 1.0;
+    textView.layer.borderColor = [UIColor colorWithRed:220/255.0 green:220/255.0 blue:220/255.0 alpha:1].CGColor;
+    textView.layer.cornerRadius = 5.0;
+    textView.text = @"区块链技术自 2017年以来备受关注。目前区块链技术的应用已经从单一的数字货币应用延伸到经济社会的各个领域。面对区块链这场“技术革命”，SegmentFault将以黑客马拉松的方式，和开发者们共同探索区块链，见证这场 Geek 盛会。";
+    textView.delegate = self;
+    [self.view addSubview:textView];
+//    labelAbstract标签与textView之间的距离
+    CGFloat labelAbstractTextViewSpace = 30;
+    UILabel *labelAbstract = [[UILabel alloc] initWithFrame:CGRectMake(textView.frame.origin.x, textView.frame.origin.y - labelAbstractTextViewSpace, 103, 21)];
+    labelAbstract.text = @"Abstract:";
+    [self.view addSubview:labelAbstract];
 }
 
+-(void)onButtonOnclick:(id)sender {
+    NSLog(@"\"点我啊\"被点击了!");
+    self.label.text = self.flag ? @"我脏了" : @"Caico Leung";
+    self.flag = !self.flag;
+}
 
 @end
